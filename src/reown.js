c8b59a34345ca6325fe2b89f3772048f2daf3579
LandoCrissian <landon361@gmail.com>
2025-05-01 13:04:30 -0600

    Update reown.js

--- src/reown.js
+++ src/reown.js
@@ -1,5 +1,8 @@
 import { createAppKit } from '@reown/appkit';
+import { wagmiAdapter } from '@reown/appkit-adapter-wagmi';
+import { solanaAdapter } from '@reown/appkit-adapter-solana';
 
 export const appKit = createAppKit({
-  projectId: '36c402e4989b8426f99d329788a526fc'
+  projectId: "36c402e4989b8426f99d329788a526fc",
+  wallets: [wagmiAdapter(), solanaAdapter()],
 });
