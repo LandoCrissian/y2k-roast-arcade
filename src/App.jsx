db90dd3fd023b2bf7b16b505aafce6d6ec6d5048
LandoCrissian <landon361@gmail.com>
2025-05-01 06:49:45 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
 
+export default function App() {
+  return (
+    <div className="crt-screen">
+      <h1>INSERT TOKEN TO ENTER THE ARCADE</h1>
+      <button>Connect Wallet</button>
+    </div>
+  );
+}
