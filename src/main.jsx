2996e60d8faac8653022aa6c1a40556a06ffdec0
LandoCrissian <landon361@gmail.com>
2025-05-01 15:03:02 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/App.css';
 
+console.log('React root loaded');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
