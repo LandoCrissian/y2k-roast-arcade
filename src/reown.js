5e70449ad88c68ae90a71c958c1fc20bae20928a
LandoCrissian <landon361@gmail.com>
2025-05-02 06:53:48 -0600

    Delete src directory

--- src/reown.js
+++
@@ -1,8 +0,0 @@
-import { createAppKit } from '@reown/appkit';
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
-import { SolanaAdapter } from '@reown/appkit-adapter-solana';
-
-export const appKit = createAppKit({
-  projectId: import.meta.env.VITE_REOWN_PROJECT_ID,
-  adapters: [new WagmiAdapter(), new SolanaAdapter()]
-});
